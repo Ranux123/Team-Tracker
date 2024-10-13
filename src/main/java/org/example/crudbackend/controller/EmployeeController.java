da6c37cc4b244349a60bc53ebc1ed7577bd9224d
ranuthd <ranuthofficial@gmail.com>
2024-10-14 00:48:34 +0530

    Mapped the getEmployeeById GET request in the EmployeeController.java

--- src/main/java/org/example/crudbackend/controller/EmployeeController.java
+++ src/main/java/org/example/crudbackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package org.example.crudbackend.controller;
 
+import org.example.crudbackend.exception.ResourceNotFoundException;
 import org.example.crudbackend.model.Employee;
 import org.example.crudbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +26,14 @@ public class EmployeeController
   public Employee createEmployee(@RequestBody Employee employee)
   {
     return employeeRepository.save(employee);
+  }
+
+  @GetMapping("employees/{id}")
+  public ResponseEntity<Employee> getEmployeeById( @PathVariable Long id)
+  {
+    Employee employee = employeeRepository.findById( id ).orElseThrow(
+            () -> new ResourceNotFoundException( "Employee not found with id: " + id ) );
+
+    return ResponseEntity.ok( employee );
   }
 }
